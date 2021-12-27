0912bb7639550c691bb7bcb1f4d701df8e91133f
kanamycine <jinyeong5320@gmail.com>
2021-12-28 00:15:42 +0900

    refactor(OutputView) : method depth 줄이기

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -7,18 +7,25 @@ public class OutputView {
 	public static void printPairCrews(Set<Set> pairCrews) {
 		System.out.println();
 		System.out.println("페어 매칭 결과입니다.");
-		Iterator iterator = pairCrews.iterator();
 		for (Set set : pairCrews) {
 			Iterator nestedIterator = set.iterator();
 			while (nestedIterator.hasNext()) {
-				if (set.size() == 2) {
-					System.out.println(nestedIterator.next() + " : " + nestedIterator.next());
-				}
-				if (set.size() == 3) {
-					System.out.println(
-							nestedIterator.next() + " : " + nestedIterator.next() + " : " + nestedIterator.next());
-				}
+				twoPeopleCrewPrint(set, nestedIterator);
+				threePeopleCrewPrint(set, nestedIterator);
 			}
+		}
+	}
+
+	private static void threePeopleCrewPrint(Set set, Iterator nestedIterator) {
+		if (set.size() == 3) {
+			System.out.println(
+					nestedIterator.next() + " : " + nestedIterator.next() + " : " + nestedIterator.next());
+		}
+	}
+
+	private static void twoPeopleCrewPrint(Set set, Iterator nestedIterator) {
+		if (set.size() == 2) {
+			System.out.println(nestedIterator.next() + " : " + nestedIterator.next());
 		}
 	}
 
